92217215e052be47b8480e931e2b694ed32988e8
Cícero <Cicero.atta@Gmail.com>
2026-07-16 21:18:26 -0300

    dia 1 21:18

---
+++ exercícios/ex_4.py
@@ -0,0 +1,23 @@
+nome = input("Digite o seu nome:")
+idade = int(input("Digite a sua idade:"))
+espaco = False
+quantidade = 0
+if (nome and idade) == True:
+    print(f"Seu nome é {nome}")
+    nome_invertido = nome[::-1]
+    print(f"Seu nome invertido é {nome_invertido}")
+    if " " in nome:
+        espaco = True
+        print("Seu nome tem espaços")
+    else:
+        print("Seu nome não tem espaços")
+    if espaco == True:
+        quantidade = len(nome) - 1
+    else:
+        quantidade = len(nome)
+    print(f"Seu nome tem {quantidade} letras")
+    print(f"A primeira letra do seu nome é {nome[0]}")
+    print(f"A ultima letra do seu nome é {nome[-1]}")
+else:
+    print("Desculpe, você deixou algum campo vazio")
+    
